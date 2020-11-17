c4c55496cb79d6d7ee495bc97e96c510ebd9c1b5
Juan Garcia <juangarcia@juans-mbp.speedport_w_724v_typ_a_05011603_06_003>
2020-11-17 11:24:31 +0100

    standard metrics for all models V1.0

--- Support_functions/utils.py
+++ Support_functions/utils.py
@@ -4,6 +4,8 @@ This script contains a set of functions that are model agnostic
 
 import pandas as pd
 import numpy as np
+import datetime
+from sklearn.metrics import mean_absolute_error
 
 def train_val_test_split(df, target_vars, val_days, test_days):
     '''
@@ -28,8 +30,14 @@ def train_val_test_split(df, target_vars, val_days, test_days):
     rows_for_test = test_days*96
     rows_for_val = val_days*96
 
-    df_features = df.drop(columns=target_vars).copy()
+    target_vars_for_test = target_vars.copy()
+    target_vars_for_test.append('Date')
+
+    df_features = df.drop(columns=target_vars_for_test).copy()
     df_target = df[target_vars].copy()
+    df_target_test = df[target_vars_for_test].copy()
+
+    del df
 
     X_train = df_features.iloc[:(df_features.shape[0]-rows_for_test-rows_for_val)]
     y_train = df_target.iloc[:(df_target.shape[0]-rows_for_test-rows_for_val)]
@@ -46,7 +54,7 @@ def train_val_test_split(df, target_vars, val_days, test_days):
     print('--------------------------------------------')
 
     X_test = df_features.iloc[X_val.shape[0]+X_train.shape[0]:]
-    y_test = df_target.iloc[y_val.shape[0]+X_train.shape[0]:]
+    y_test = df_target_test.iloc[y_val.shape[0]+X_train.shape[0]:]
     print('\n--------------------------------------------')
     print('The shape of the test set is: {}'.format(X_test.shape))
     print('The shape of the target variable for the test set is: {}'.format(y_test.shape))
@@ -54,7 +62,77 @@ def train_val_test_split(df, target_vars, val_days, test_days):
 
     return X_train, y_train, X_val, y_val, X_test, y_test
 
-'''
-def model_metrics(y_prediction, y_true):
-    give me the mae of my model and compare it to entso-e and maybe the baseline - in best case use a graph
-'''
+
+def get_model_metrics(y_true_with_date, y_prediction):
+
+    # get the total error in MW
+
+    y_true = np.array(y_true_with_date.iloc[:,1])
+    y_pred = np.array(y_prediction)
+
+    total_pred = sum(y_pred)
+    total_true = sum(y_true)
+    absolute_error = total_true - total_pred
+
+    print('\n----------------------------------------------')
+    print(f'The absolute error (total actual minus  forecast) in MW is: {round(absolute_error, 2)}')
+    print('----------------------------------------------\n')
+
+    # get the overall MAE
+
+    overall_mae = mean_absolute_error(y_true, y_pred)
+
+    print('\n----------------------------------------------')
+    print(f'The overall mean absolute error of the model in MW is: {overall_mae}')
+    print('----------------------------------------------\n')
+
+    # get the overall mean absolute scaled error (MASE)
+
+    naive_forecast = y_true[1:]
+    y_true_for_mase = y_true[:-1]
+    mae_naive = mean_absolute_error(y_true_for_mase, naive_forecast)
+    overall_mae_without_first_observation = mean_absolute_error(y_true[1:], y_pred[1:])
+
+    overall_mase = overall_mae_without_first_observation/mae_naive
+
+    print('\n----------------------------------------------')
+    print(f'The overall mean absolute scaled error of the model in MW is: {overall_mase}')
+    print('Please note: to calculate the MASE, the prediction for the first observation was omitted')
+    print('----------------------------------------------\n')
+
+    # get the MAE for every day and return a dataframe and charts
+
+    time_stamp = np.array(
+                 pd.to_datetime(y_true_with_date.iloc[:,0], format='%Y-%m-%d %H:%M:%S').dt.date
+                 )
+
+    date_list = list()
+    y_true_list = list()
+    y_pred_list = list()
+
+    df = pd.DataFrame({'date': time_stamp,
+                       'y_true': y_true,
+                       'y_pred': y_pred})
+
+    list_of_days = sorted(list(set(df.date.values)))
+
+    for day in list_of_days:
+        sub_df = df.query('date == @day')
+        date_list.append(sub_df.date.values)
+        y_true_list.append(sub_df.y_true.values)
+        y_pred_list.append(sub_df.y_pred.values)
+
+    mae_list = list()
+    for i, j, k in zip(y_true_list, y_pred_list, date_list):
+        MAE = mean_absolute_error(i, j)
+        mae_list.append(MAE)
+
+    del df
+
+    df = pd.DataFrame({'day':list_of_days, 'MAE': mae_list},)
+
+    print('\n----------------------------------------------')
+    print('This function also returns a dataframe with the MAE for each day')
+    print('----------------------------------------------\n')
+
+    return df
