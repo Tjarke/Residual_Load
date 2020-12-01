63d1b4ff46033cc73a6efcb01208105116888732
Tjarke <Tjarke@web.de>
2020-12-01 11:18:51 +0100

    added feature channels for installed capacity

--- Support_functions/utils.py
+++ Support_functions/utils.py
@@ -5,6 +5,7 @@ This script contains a set of functions that are model agnostic
 import pandas as pd
 import numpy as np
 import datetime
+import time
 from sklearn.metrics import mean_absolute_error
 
 def train_val_test_split(df, target_vars, val_days, test_days):
@@ -195,17 +196,17 @@ def create_feature_channels(df,stations,lat_steps,lon_steps,features,fillvalue=n
     This function will create "Pictures" where the pictures of the image correspond to the geographical location based on latitude and longitude 
     The value of each pixel corresponst to the value of the feature for that channel
     
-    Parameters
+    The input Parameters:
     ----------
-    df : TYPE
+    df : TYPE pandas dataframe
         df containing the features for different tables, named according to our convention
-    stations : TYPE
+    stations : TYPE pandas dataframe
         a dataframe containing the id and latitude and longitude of the stations used
-    lat_steps : TYPE
+    lat_steps : TYPE int
         amouunt indicates the amount of pixels in the y-direction
-    lon_steps : TYPE
+    lon_steps : TYPE int
         amouunt indicates the amount of pixels in the x-direction
-    features : TYPE
+    features : TYPE list of strings
         a list of features we want to include as feature channels
 
     Returns
@@ -239,5 +240,101 @@ def create_feature_channels(df,stations,lat_steps,lon_steps,features,fillvalue=n
                     if (df.filter(regex=station_string[:-1]).filter(regex=l).shape[1]>0):
                         feature_channels[i,j,cnt,:] = df.filter(regex=station_string[:-1]).filter(regex=l).mean(axis=1)
 
+    
+    return feature_channels
+
+### feature engineering for the installed capacity
+
+def create_feature_channels_installed_capacity(df,date_column,lat_steps,lon_steps,time_res,features=["Wind","Solar"],suppress=True):
+    '''
+    This function takes as a dataframe containing the installed capacity in germany and returns a grid with feature channels
+    Also see create_feature_channels
+    
+    The input Parameters:
+    ----------
+    df : TYPE pandas dataframe
+        Dataframe containing the installed capacity per type in Germany
+    date_column : TYPE pandas series
+        a pandas series with datetime objects containing the range of dates the feature channels should span
+    lat_steps : TYPE int
+        amouunt indicates the amount of pixels in the y-direction
+    lon_steps : TYPE int
+        amouunt indicates the amount of pixels in the x-direction
+    time_res : TYPE int
+        number of timesteps per day e.g. 96 for 15 min resolution
+    features : TYPE, optional list of strings
+        Gives the capacity types we want to create feature channels for
+    suppress : TYPE, optional boolean
+        set to True if print statements throughout the operation are wanted
+
+    Returns
+    -------
+    feature_channels : numpy 4d tensor
+        The feature channel "picture" of Germany
+
+    '''
+    df.loc[:,"commissioning_date"] = pd.to_datetime(df.loc[:,"commissioning_date"], format='%Y-%m-%d')
+    df.loc[:,"decommissioning_date"].fillna("01.01.2055",inplace=True)
+    df.loc[:,"decommissioning_date"] = pd.to_datetime(df.loc[:,"decommissioning_date"], format='%d.%m.%Y')
+    
+    time_steps = date_column.shape[0]
+    amount_of_features = len(features)
+    
+    lat_range = [47.3,55]
+    lon_range = [5.625,15]
+    
+    stepsize_lat = (lat_range[-1]-lat_range[0])/(lat_steps-1)
+    stepsize_lon = (lon_range[-1]-lon_range[0])/(lon_steps-1)
+    
+
+    feature_channels = np.zeros((lat_steps,lon_steps,amount_of_features,time_steps),"float32")
+    
+    start = time.time()
+
+
+
+
+    df_dict = {}
+    
+    for i in range(lat_steps):
+        start_loop = time.time()
+        df_dict[i] = {}
+        for j in range(lon_steps):
+            df_dict[i][j] = {}
+            lat_sub = [lat_range[0]+stepsize_lat*i,lat_range[0]+stepsize_lat*(i+1)]
+            lon_sub = [lon_range[0]+stepsize_lon*j,lon_range[0]+stepsize_lon*(j+1)]
+    
+            for cnt,l in enumerate(features):
+                select_stations = (df.lat > lat_sub[0]) & (df.lat <= lat_sub[1]) \
+                                  &(df.lon > lon_sub[0]) & (df.lon <= lon_sub[1]) \
+                                  &(df.energy_source_level_2 == l)
+    
+                df_dict[i][j][l] = df.loc[select_stations,["electrical_capacity","commissioning_date","decommissioning_date"]]
+        end_loop = time.time()
+        if not suppress:
+            print(f"The loop took {round(end_loop - start_loop,2)}s step {i+1} out of {lat_steps}")
+    if not suppress:                
+        end = time.time()
+        print(f"Creating the dictionary took {round(end - start,2)}s")
+    
+    for t in range(int(time_steps/time_res)-1):
+        start_loop = time.time()
+        date = date_column.iloc[time_res*t]
+        for i in range(lat_steps):
+            for j in range(lon_steps):
+                for cnt,l in enumerate(features):
+                    if df_dict[i][j][l].shape[0] > 0:
+                        select_stations = (df_dict[i][j][l].commissioning_date <= date) \
+                                           & (df_dict[i][j][l].decommissioning_date > date) 
+                        feature_channels[i,j,cnt,time_res*t:time_res*(t+1)] = df_dict[i][j][l].loc[select_stations,"electrical_capacity"].sum()
+                    else:
+                        feature_channels[i,j,cnt,time_res*t:time_res*(t+1)] = 0
+        if not suppress:
+            end_loop = time.time()
+            print(f"the loop took{round(end_loop - start_loop,2)} , step {t} of {int(time_steps/time_res)-1}")
+
+    if not suppress:
+        end = time.time()
+        print(f"Creating the feature channels took {round(end - start,2)}s")
     
     return feature_channels
